0f610d3f4c244fe7caad25fc5024e728b518fb4c
Kensu Desu <Kensu90@gmail.com>
2018-03-17 12:48:46 +0100

    Add project files.

---
+++ CalculatorAPI/Controllers/Add.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class Add : Controller
+    {
+        // GET api/Add
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/Add/5/5
+        [HttpGet("{num1}{num2}")]
+        public int Get(int num1, int num2)
+        {
+            return num1+num2;
+        }
+
+        // POST api/Add
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/Add/5
+        [HttpPut("{num1}")]
+        public void Put(int num1, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/Add/5
+        [HttpDelete("{sum}")]
+        public void Delete(int sum)
+        {
+        }
+    }
+}
---
+++ CalculatorAPI/Models/Numbers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalculatorAPI.Models
+{
+    public class Numbers
+    {
+        public long Num1 { get; set; }
+        public long Num2 { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
